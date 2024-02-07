6014c2a6acc8fdd30c2c72a79bcd08551012f700
TagriffUldan <brandon.nicklas@outlook.com>
2024-02-06 21:23:28 -0800

    added tests and initial folder structure

---
+++ src/protocol/protocol.py
@@ -0,0 +1,6 @@
+@dataclass
+class SimpleString:
+    data: str
+
+def extract_frame_from_buffer(buffer):
+    return None, 0
---
+++ tests/test_protocol.py
@@ -0,0 +1,19 @@
+from src.protocol import SimpleString, extract_frame_from_buffer
+
+def test_read_frame_simple_string_incomplete_frame():
+    buffer = b"+Par"
+    frame, frame_size = extract_frame_from_buffer(buffer)
+    assert frame == None
+    assert frame_size == 0
+
+def test_read_frame_simple_string_complete_frame():
+    buffer = b"+OK\r\n"
+    frame, frame_size = extract_frame_from_buffer(buffer)
+    assert frame == SimpleString("OK")
+    assert frame_size == 5
+
+def test_read_frame_simple_string_extra_data():
+    buffer = b"+OK\r\n+Next"
+    frame, frame_size = extract_frame_from_buffer(buffer)
+    assert frame == SimpleString("OK")
+    assert frame_size == 5
